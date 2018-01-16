5a1cddac8b47e096b8eba50aadd854229d61f1ad
MollyRoseKG <33360068+MollyRoseKG@users.noreply.github.com>
2018-01-16 16:56:29 -0500

    Added debug as an input
    If debug is set to a nonzero scalar the program will produce plots of huricane H matrix and the line.

--- one-bus-resilience/hurricane2d.m
+++ one-bus-resilience/hurricane2d.m
@@ -1,6 +1,11 @@
-function [ H ] = hurricane2d( n,M,r )
-%UNTITLED Summary of this function goes here
+function [ H ] = hurricane2d( n,M,r,debug )
+%UNTITLED [ H ] = hurricane2d( n,M,r,debug )
 %   Detailed explanation goes here
+
+if nargin < 4
+    debug = 0;
+end
+
 D = zeros(n);
 m = randi(n,[1,4]); % random 2 x and y points that are on the line of the huricane
 x = [m(1); m(2)]; % sets first two random integers as x coordinates
@@ -18,6 +23,7 @@ for ii=1:n
 end
 R = 10*r; % can use to scale r to the proper spacial dimentions
 H = M.*(exp(-D./R));
+if debug
 figure
 contour(H)
 hold on
@@ -26,5 +32,6 @@ title('Huricane Strength over Map')
 xlabel('x location')
 ylabel('y loaction')
 hold off
+end
 end
 
