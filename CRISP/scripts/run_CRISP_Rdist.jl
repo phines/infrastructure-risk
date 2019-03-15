242d7a4d5ed463d17b3ead87b1739f6d72ca4cc0
MollyRoseKG <33360068+MollyRoseKG@users.noreply.github.com>
2019-03-15 10:56:30 -0400

    results

--- CRISP/scripts/run_CRISP_Rdist.jl
+++ CRISP/scripts/run_CRISP_Rdist.jl
@@ -1,12 +1,12 @@
 include("..\\src\\CRISP_Rdist.jl")
 ## folder of case data
-case = "data\\saved_ps\\case39_30PV\\"
+case = "data\\saved_ps\\case39_05PV\\"
 # number of events
 N = 1000;
 
 #save restoration data to folder within results folder:
 filename = "resilience_out1";
-out_folder = "\\case39_30PV\\$filename.csv"
+out_folder = "\\case39_05PV\\$filename.csv"
 
 # run to save csv of resilience cost distribution to the specified out_folder
 res = Res_dist(N,case,out_folder)
