60ead4db605bb10a1410006d019cdcf80505e4e1
MollyRoseKG <33360068+MollyRoseKG@users.noreply.github.com>
2019-10-06 17:13:39 -0400

    edited figures

--- CRISP/matlab/square_plots.m
+++ CRISP/matlab/square_plots.m
@@ -6,9 +6,9 @@ end
 n_sets = size(data,1);
 
 
-if any(any(data<0))
-    error('Does not work for <0 data');
-end
+%if any(any(data<0))
+    %error('Does not work for <0 data');
+%end
 if prod(arrangement)~=n_sets
     error('Bad dimensions');
 end
@@ -30,29 +30,41 @@ for row = 1:n_sets
     center_xy = all_centers(row,:);
     % find the patch dimensions
     % lower left
+    if data(row,1)<=0
+    else
     area = data(row,1);
     side = sqrt(area);
     lower_left_xy = center_xy - side;
     pos = [lower_left_xy side side];
     rectangle('Position',pos,'FaceColor',colors(1,:),'EdgeColor','none');
+    end
     % lower right
+    if data(row,2)<=0
+    else
     area = data(row,2);
     side = sqrt(area);
     lower_left_xy = center_xy - [0 side];
     pos = [lower_left_xy side side];
     rectangle('Position',pos,'FaceColor',colors(2,:),'EdgeColor','none');
+    end
     % upper left
+    if data(row,3)<=0
+    else
     area = data(row,3);
     side = sqrt(area);
     lower_left_xy = center_xy - [side 0];
     pos = [lower_left_xy side side];
     rectangle('Position',pos,'FaceColor',colors(3,:),'EdgeColor','none');
+    end
     % upper right
+    if data(row,4)<=0
+    else
     area = data(row,4);
     side = sqrt(area);
     lower_left_xy = center_xy;
     pos = [lower_left_xy side side];
     rectangle('Position',pos,'FaceColor',colors(4,:),'EdgeColor','none');
+    end
 end
 
 axis off;
