f0bc151e8255d08158ac92f39a1446290ef60fce
MollyRoseKG <33360068+MollyRoseKG@users.noreply.github.com>
2020-01-30 11:22:18 -0500

    working on casc-rest colab

--- CRISP/scripts/debugging_CRISP.jl
+++ CRISP/scripts/debugging_CRISP.jl
@@ -15,13 +15,24 @@ include("..\\src\\CRISP_RLSOPF.jl")
 include("..\\src\\CRISP_RT.jl")
 include("..\\src\\CRISP_network.jl")
 ## load the case data
+ex = 1
 ps = import_ps("data/saved_ps/case2736sp_relaxedQ_ps")
-ps1 = import_ps("data/saved_ps/case2736sp_relaxedQ_ps_ex2")
+ps.branch.status = ones(length(ps.branch.status))
+ps.gen.status = ones(length(ps.gen.status))
+ps1 = import_ps("data/saved_ps/2736sp_relaxedQ_ps_ex$ex")
 ps.shunt = ps.shunt[ps.shunt.P .!=0.0,:]
-crisp_dcpf_g1_s!(ps)
+for s in 1:length(ps.shunt.bus)
+    if (sum(ps1.shunt.bus .== ps.shunt.bus[s]) == 0)
+        ps.shunt.status[s] = 0
+    else
+        ps.shunt.status[s] = (ps1.shunt.P[ps1.shunt.bus .== ps.shunt.bus[s]][1]) / ps.shunt.P[s]
+    end
+end
+#crisp_dcpf_g1_s!(ps)
 total = sum(ps.shunt.P);
 Pd_max = deepcopy(ps.shunt.P);
 gen_on = ps.gen.Pg .!= 0;
+ps.gen.Pg = ps1.gen.Pg
 # parameters
 param_file = ""
 if isempty(param_file)
@@ -33,23 +44,24 @@ end
 #save restoration picture to:
 filename = "Polish_Case";
 #pull failures from csv file
-Lines_Init_State = CSV.File("data/example_outages/lines_tripped_ex2.csv") |> DataFrame
+Lines_Init_State = CSV.File("data/example_outages/lines_tripped_ex$ex.csv") |> DataFrame
 nl = size(ps.branch,1)
 NLines = length(Lines_Init_State.lines_tripped);
 ps.branch.status[Int64.(Lines_Init_State.lines_tripped)] = zeros(NLines);
 RecovTimeL = RecoveryTimes(mu_line, sigma_line, NLines);
 l_recovery_times = RecTime(RecovTimeL, ps.branch.status).recovery_time;
-Gens_Init_State = CSV.File("data/example_outages/gens_tripped_ex2.csv") |> DataFrame
+Gens_Init_State = CSV.File("data/example_outages/gens_tripped_ex$ex.csv") |> DataFrame
 ng = size(ps.gen,1);
 NGens = length(Gens_Init_State.gens_tripped);
 ps.gen.status[Int64.(Gens_Init_State.gens_tripped)] = zeros(NGens);
-RecovTimeG = RecoveryTimes(mu_line, sigma_line, NGens);
+#RecovTimeG = RecoveryTimes(mu_line, sigma_line, NGens);
 g_recovery_times = RecTime(RecovTimeG, ps.gen.status).recovery_time;
+g_recovery_times[g_recovery_times .!= 0] = ones(sum(g_recovery_times .!= 0))
 ## run step 3
 dt = 60
 ti = 60*48;
 t0 = 0
 Restore = crisp_RLOPF_v1(ps,l_recovery_times,g_recovery_times,dt,ti,t0,gen_on)
-CSV.write("results\\Restoration$(filename)_ex2.csv",Restore)
+CSV.write("results\\Restoration$(filename)_ex$ex.csv",Restore)
 ## run step 4
 ResilienceTri = crisp_res(Restore);
--- CRISP/src/CRISP_RLSOPF.jl
+++ CRISP/src/CRISP_RLSOPF.jl
@@ -3106,15 +3106,15 @@ function crisp_RLOPF_v1(ps,l_recovery_times,g_recovery_times,dt,t_window,t0,gen_
     load_shed = sum(load_cost.*(ps.shunt.P - ps.shunt.P.*ps.shunt.status));
     perc_load_served = (sum(load_cost.*ps.shunt.P) .- load_shed)./sum(load_cost.*ps.shunt.P);
     lines_out = length(ps.branch.status) - sum(ps.branch.status);
-    gens_out = length(ps.gen.status) - sum(ps.gen.status);
+    gens_off = length(ps.gen.status) - sum(ps.gen.Pg > ps.gen.Pmin); #sum(ps.gen.status);#
     Restore = DataFrame(time = ti, load_shed = load_shed, perc_load_served = perc_load_served,
-    lines_out = lines_out, gens_out = gens_out)
+    lines_out = lines_out, gens_off = gens_off)
     cv = deepcopy(Restore);
     # find generator status
     ug = gen_on_off2(ps,Time,t_window,gen_on,g_recovery_times)
     # find line status
     ul = line_stats(ps,Time,t_window,l_recovery_times)
-    for i in 2:length(Time)
+    for i in 1:length(Time)
         # update time
         ti = Time[i]-t0;
         # remove failures as the recovery time is reached
@@ -3141,7 +3141,7 @@ function crisp_RLOPF_v1(ps,l_recovery_times,g_recovery_times,dt,t_window,t0,gen_
         cv.load_shed .= sum(load_cost.*(ps.shunt.P - ps.shunt.P.*ps.shunt.status));
         cv.perc_load_served .= (sum(load_cost.*ps.shunt.P) .- cv.load_shed)./sum(load_cost.*ps.shunt.P);
         cv.lines_out .= length(ps.branch.status) - sum(ps.branch.status);
-        cv.gens_out .= length(ps.gen.status) - sum(ps.gen.status);
+        cv.gens_off .= length(ps.gen.status) - sum(ps.gen.Pg > ps.gen.Pmin); #sum(ps.gen.status);
         append!(Restore,cv)
         @assert 10^(-4)>=abs(sum(ps.shunt.P .* ps.shunt.status)-sum(ps.storage.Ps)-sum(ps.gen.Pg))
     end
