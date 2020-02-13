4b2c26d176f728b627d92aca5b923759d970e3bb
MollyRoseKG <33360068+MollyRoseKG@users.noreply.github.com>
2020-02-13 16:22:54 -0500

    working on bad get worse exp to logn

--- CRISP/matlab/testing_exp2lognorm_figure.m
+++ CRISP/matlab/testing_exp2lognorm_figure.m
@@ -1,20 +1,24 @@
-r1 = xlsread('..\results\test_exponential2lognormal_p1_0.1.csv');
-r2 = xlsread('..\results\test_exponential2lognormal_p1_0.2.csv');
-r3 = xlsread('..\results\test_exponential2lognormal_p1_0.3.csv');
-r4 = xlsread('..\results\test_exponential2lognormal_p1_0.4.csv');
-r5 = xlsread('..\results\test_exponential2lognormal_p1_0.5.csv');
+r1 = xlsread('..\results\test_exponential2lognormal_p1_0.3.csv');
+r2 = xlsread('..\results\test_exponential2lognormal_p1_0.4.csv');
+r3 = xlsread('..\results\test_exponential2lognormal_p1_0.5.csv');
+r4 = xlsread('..\results\test_exponential2lognormal_p1_0.8.csv');
+r5 = xlsread('..\results\test_exponential2lognormal_p1_0.9.csv');
+r6 = xlsread('..\results\test_exponential2lognormal_p1_1.0.csv');
+r7 = xlsread('..\results\test_exponential2lognormal_p1_1.1.csv');
 results0 = r1(:,1);
 results1 = r1(:,2);
 results2 = r2(:,2);
 results3 = r3(:,2);
 results4 = r4(:,2);
 results5 = r5(:,2);
-n = length(results);
+results6 = r6(:,2);
+results7 = r7(:,2);
+n = length(results0);
 
 % figure(1); clf;
 % hist(results1,20);
 % hold on
-% hist(results,20,'r');
+% hist(results1,20,'r');
 % legend('lognormal(3.22,2.43)','weibull(1000,10),factor 1.5')
 % hold off
 
@@ -24,32 +28,31 @@ p = (n:-1:1)/n;
 loglog(x,p);
 hold on
 x = sort(results1);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results2);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results3);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results4);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results5);
-p = (n:-1:1)/n;
 loglog(x,p);
-legend('lognormal(3.22,2.43)','weibull(0.1,10),factor 1.1',...
-    'weibull(0.2,10),factor 1.1','weibull(0.3,10),factor 1.1',...
-    'weibull(0.4,10),factor 1.1','weibull(0.5,10),factor 1.1')
+x = sort(results6);
+loglog(x,p);
+x = sort(results7);
+loglog(x,p);
+legend('lognormal(3.22,2.43)','weibull(0.3,10),factor 1.1',...
+    'weibull(0.4,10),factor 1.1','weibull(0.5,10),factor 1.1',...
+    'weibull(0.8,10),factor 1.1','weibull(0.9,10),factor 1.1','weibull(1.0,10)','weibull(1.1,10)')
 hold off
 
 %% p2
 
-r1 = xlsread('..\results\test_exponential2lognormal_p2_15.csv');
-r2 = xlsread('..\results\test_exponential2lognormal_p2_20.csv');
-r3 = xlsread('..\results\test_exponential2lognormal_p2_25.csv');
-r4 = xlsread('..\results\test_exponential2lognormal_p2_30.csv');
-r5 = xlsread('..\results\test_exponential2lognormal_p2_40.csv');
+r1 = xlsread('..\results\test_exponential2lognormal_p2_40.csv');
+r2 = xlsread('..\results\test_exponential2lognormal_p2_45.csv');
+r3 = xlsread('..\results\test_exponential2lognormal_p2_50.csv');
+r4 = xlsread('..\results\test_exponential2lognormal_p2_60.csv');
+r5 = xlsread('..\results\test_exponential2lognormal_p2_70.csv');
 results0 = r1(:,1);
 results1 = r1(:,2);
 results2 = r2(:,2);
@@ -63,21 +66,20 @@ p = (n:-1:1)/n;
 loglog(x,p);
 hold on
 x = sort(results1);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results2);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results3);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results4);
-p = (n:-1:1)/n;
 loglog(x,p);
 x = sort(results5);
-p = (n:-1:1)/n;
 loglog(x,p);
-legend('lognormal(3.22,2.43)','weibull(0.3,15),factor 1.1',...
-    'weibull(0.3,20),factor 1.1','weibull(0.3,25),factor 1.1',...
-    'weibull(0.3,30),factor 1.1','weibull(0.3,40),factor 1.1')
+%legend('lognormal(3.22,2.43)','weibull(1,15),factor 1.1',...
+    %'weibull(1,20),factor 1.1','weibull(1,25),factor 1.1',...
+    %'weibull(1,30),factor 1.1','weibull(1,40),factor 1.1')
+
+legend('lognormal(3.22,2.43)','weibull(1,40),factor 1.1',...
+    'weibull(1,45),factor 1.1','weibull(1,50),factor 1.1',...
+    'weibull(1,60),factor 1.1','weibull(1,70),factor 1.1')
 hold off
--- CRISP/scripts/text_exp2lognorm.jl
+++ CRISP/scripts/text_exp2lognorm.jl
@@ -6,17 +6,17 @@ include("../src/CRISP_initiate.jl")
 N = 100000
 mu = 3.66
 sigma = 2.43
-P1 = 0.1:0.1:0.5
-p2 = 10
-factor = 1.1
+P1 = 0.1:0.1:1.5
+p2 = 50
+factor = 1.15
 rest_times1 = RecoveryTimes(mu,sigma,N)
 for p1 in P1
     rest_times2 = exp2lognorm(N,p1,p2,factor)
     results = DataFrame(log_normal_dist = rest_times1, weibull_gone_bad = rest_times2)
     CSV.write("results\\test_exponential2lognormal_p1_$p1.csv",results)
 end
-p1 = 0.3
-P2 = 15:5:45
+p1 = 1.0
+P2 = 50:10:70
 for p2 in P2
     rest_times2 = exp2lognorm(N,p1,p2,factor)
     results = DataFrame(log_normal_dist = rest_times1, weibull_gone_bad = rest_times2)
